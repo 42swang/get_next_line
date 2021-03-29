5fc9bf28d87d7bb5debe87bfd51c97c771b8f7a4
swang <swang@MacBook-sh.local>
2021-03-29 18:35:37 +0900

    210329 test all pass in local

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # get_next_line
+
+norminette v3 기준으로 작성된 코드입니다.  
+테스트는 42seoul wiki get-next-line페이지에 있는 스크립트를 사용하였습니다.
+mandatory파트와 bonus파트 모두 구현하였습니다.
+
+---
+
+개인맥(M1)환경에서 테스트한 결과
+
+norminette v3 [OK]
+
+|통과여부|테스터기이름|
+|:----:|:------:|
+|[x]|42testers-gnl|
+|[x]|gnl-lover|
+|[x]|gnlkiller|
+|[x]|gnlkiller2|
+|[x]|gnlTester|
+|[x]|Test-42|
+
+---
+
+ssh환경에서 테스트한 결과
+
+|통과여부|테스터기이름|
+|:----:|:------:|
+|[]|42testers-gnl|
+|[]|gnl-lover|
+|[]|gnlkiller|
+|[]|gnlkiller2|
+|[]|gnlTester|
+|[]|Test-42|
---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/27 14:03:14 by swang             #+#    #+#             */
+/*   Updated: 2021/03/28 18:07:25 by swang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define OPEN_MAX 256
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strnl(const char *s);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+
+int		get_next_line(int fd, char **line);
+
+#endif
